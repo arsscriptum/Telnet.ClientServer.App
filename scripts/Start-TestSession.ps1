caa089c9bab0f4ddb14217e2300b8a9dedfef2ac
arsscriptum <guillaumeplante.qc@gmail.com>
2023-04-16 20:58:32 -0400

    auto

--- scripts/Start-TestSession.ps1
+++ scripts/Start-TestSession.ps1
@@ -4,15 +4,29 @@ function Get-ScriptDirectory {
     Split-Path -Parent $PSCommandPath
 }
 
+
+$ServerStartScript = {
+    param($Path,$Mode)
+  
+    try{
+        &"$Path" "$Mode"
+    }catch{
+        Write-Output "$_"
+    }
+}.GetNewClosure()
+
+[scriptblock]$ServerStartScriptBlock = [scriptblock]::create($ServerStartScript) 
+
 function Start-TestSession {
    [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory=$false)]
         [ValidateSet('Debug','Release')]
-        [string]$Configuration
+        [string]$Configuration="Debug"
     )
       try{
         $ScriptDirectory = Get-ScriptDirectory
+
         $SolutionRoot = (Resolve-Path "$ScriptDirectory\..").Path
         $OutputDirectory = (Resolve-Path "$SolutionRoot\bin\Win32\$Configuration").Path
         $ClientApp = Join-Path "$OutputDirectory" "telnet_client.exe"
@@ -20,56 +34,57 @@ function Start-TestSession {
 
         $ClientValid = Test-Path $ClientApp -PathType Leaf
         $ServerValid = Test-Path $ServerApp -PathType Leaf
+
+        if(!$ServerValid){ throw "cannot find server app" }
+        if(!$ClientValid){ throw "cannot find client app" }
+
+        $jobby = Start-Job -Name $JobName -ScriptBlock $ServerStartScriptBlock -ArgumentList ($ServerApp,"-d")
+
+        Start-Sleep 2
+
+        &"$ClientApp"
+
     }catch{
         Show-ExceptionDetails $_ -ShowStack
-       
+    }
+}
+
+
+
+function Stop-TestSession {
+   [CmdletBinding(SupportsShouldProcess)]
+    param()
+      try{
+        $ClientAppBaseName = "telnet_client"
+        $ClientApp = $ClientAppBaseName + ".exe"
+        $ServerAppBaseName = "telnet_srv"
+        $ServerApp = $ServerAppBaseName + ".exe"
+        $PlExe = (Get-Command "pl.exe").Source
+        [string[]]$Out = &"$plexe" 'telnet_srv'
+        $ServerPresent = !($Out[0].Contains('Error'))
+        [string[]]$Out = &"$plexe" 'telnet_client'
+        $ClientPresent = !($Out[0].Contains('Error'))
+        $tk=(get-command taskkill).Source
+
+        if($ServerPresent){
+            Write-Output "Killing running server instance..."
+            $Out = &"$tk" '/IM' "$ServerApp" '/f'
+        }else{
+            Write-Output "No server instances running."
+        }
+    
+        if($ClientPresent){
+            Write-Output "Killing running client instance..."
+            $Out = &"$tk" '/IM' "$ClientApp" '/f'
+        }else{
+            Write-Output "No client instances running."
+        }
+
+    }catch{
+        Show-ExceptionDetails $_ -ShowStack
     }
 }
 
 
 
 
-      try{
-
-
-        $ErrorDetails=''
-        $ErrorOccured=$False
-        $Script:Configuration = "Debug"
-        if(!([string]::IsNullOrEmpty($args[0]))){
-            $Script:Configuration = $args[0]
-        }
-        $ScriptDirectory = Get-ScriptDirectory
-        $SolutionRoot = (Resolve-Path "$ScriptDirectory\..").Path
-        $OutputDirectory = (Resolve-Path "$SolutionRoot\bin\Win32\$Configuration").Path
-        $DejaInsighDll = "F:\Development\DejaInsight\lib\DejaInsight.x86.dll"
-        Copy-Item $DejaInsighDll $OutputDirectory -Force
-        Write-Output "COPY DEJA INSIGHT DLL TO `" $OutputDirectory `""
-
-
-        if($Script:Configuration -eq "Debug"){
-            $Text = $Text -f $TextDebug
-        }else{
-            $Text = $Text -f $TextRelease
-        }
-        
-        Write-Output $Text
-
-        $dep1 = "$PSScriptRoot\dependencies\Dependencies.ps1"
-        . "$dep1"
-
-        <#$Script:Description = "Helps the computer run more efficiently by optimizing storage compression."
-        Install-WinService -Name "$Script:ServiceName" -GroupName $Script:ServiceGroup -Path $Script:ServicePath -Description $Script:Description -StartupType Automatic -SharedProcess
-        Set-ServicePermissions -Name "$Script:ServiceName" -Identity "$ENV:USERNAME" -Permission full
-        Set-ServicePermissions -Name "$Script:ServiceName" -Identity "NT AUTHORITY\SYSTEM" -Permission full
-        Set-ServicePermissions -Name "$Script:ServiceName" -Identity "NT AUTHORITY\SERVICE" -Permission full
-        Invoke-CmProtek -InputFile "$Script:TargetPath" -OutputFile "$Script:ServicePath"
-        #>
-    }catch{
-        Write-Error "$_"
-        $ErrorDetails= "$_"
-        $ErrorOccured=$True
-    }
-    if($ErrorOccured){
-        Start-Sleep 2
-        throw "$ErrorDetails"
-    }
